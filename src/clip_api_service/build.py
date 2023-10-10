bdcb56f6920e662aee55b700860086ef38bd7a0f
jianshen <jianshen92@gmail.com>
2023-10-10 17:27:35 +0800

    Fixing model name not being passed into build

--- src/clip_api_service/build.py
+++ src/clip_api_service/build.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from clip_api_service.models import MODEL_ENV_VAR_KEY
+
 import os
 
 
@@ -12,6 +14,11 @@ def build_bento(model_name: str | None = None, use_gpu: bool = False):
     clip_module = get_clip_module(bento_model)
 
     bento_file = clip_module.bentofile_path(use_gpu=use_gpu)
+
+    # Setting Model name as environment variable to pass 
+    # into subprocess for bento build
+    if model_name:
+        os.environ[MODEL_ENV_VAR_KEY] = model_name
 
     # Get parent directory path
     build_ctx = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
